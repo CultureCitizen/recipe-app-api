e78eb748809a7d3b6f9f703313a1eabdd9c820e7
Armando de la Torre <adelatorremothelet@gmail.com>
2020-11-05 14:26:23 -0600

    Added admin customization

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _
 # Register your models here.
+
+
+# The model.User class is displayed using the UserAdmin class
+admin.site.register(models.User, UserAdmin)
